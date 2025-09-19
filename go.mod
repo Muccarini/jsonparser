d25eea19e5ce348d45931b0180904176bd2023cb
Luca Pandolfini <luca.pandolfiniae@gmail.com>
2025-09-19 23:02:25 +0200

    inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/muccarini/jsonparser
+
+go 1.24.4
---
+++ jsonparser.go
@@ -0,0 +1,20 @@
+package jsonparser
+
+// IMPORT
+import (
+	"fmt"
+)
+
+// VARS
+
+// API
+func Get() {
+	fmt.Println("Hello, World!")
+
+	get()
+}
+
+// FUNC
+func get() {
+	fmt.Println("get")
+}
